2ac229087a9da76c1ce5b47afa25d5f6307fcd6e
UROUS <urous3814@gmail.com>
2025-10-29 16:37:38 +0900

    main.bash 스크립트 수정: 제출자료 폴더 경로를 학번 기반으로 변경하고, 학번 및 반 변수를 추가하여 유연성 향상

--- main.bash
+++ main.bash
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # 제출자료 폴더가 없으면 생성
-mkdir -p ./제출자료
+
+schoolNumber="2371335"  # 여기에 학번을 입력하세요
+className=D    # 여기에 반을 입력하세요
+mkdir -p ./${schoolNumber}
 
 # src/examD/ 하위의 모든 .jsx 파일을 대상으로 반복
-find ./src/examD/ -type f -name "*.jsx" | while read filepath; do
+find ./src/exam${className}/ -type f -name "*.jsx" | while read filepath; do
   # 상위 폴더 이름(숫자) 추출
   foldername=$(basename $(dirname "$filepath"))
   # 복사할 파일명: 숫자.jsx
-  dest="./제출자료/${foldername}.jsx"
+  dest="./${schoolNumber}/${foldername}.jsx"
   # 파일 복사
   cp "$filepath" "$dest"
 done
